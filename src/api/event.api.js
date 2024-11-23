94abc221158cd7d037d50bb75196a1ce586cf9d3
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-11-22 23:10:29 -0500

    Axios

--- src/api/event.api.js
+++ src/api/event.api.js
@@ -11,13 +11,13 @@ export const updateEvent = (id, data) => {
 }
 //Delete Event
 export const deleteEvent = (id) => {
-    return apiClient.delete(`https://backarsogam-production.up.railway.app//api/deleteEvent/${id}`)
+    return apiClient.delete(`https://backarsogam-production.up.railway.app/api/deleteEvent/${id}`)
 }
 //Get One Event
 export const getOneEvent = (id) => {
-    return apiClient.get(`https://backarsogam-production.up.railway.app//api/getOneEvent/${id}`)
+    return apiClient.get(`https://backarsogam-production.up.railway.app/api/getOneEvent/${id}`)
 }
 //Get All Events
 export const allEvent = () => {
-    return apiClient.get('https://backarsogam-production.up.railway.app//api/allEvents')
+    return apiClient.get('https://backarsogam-production.up.railway.app/api/allEvents')
 }
