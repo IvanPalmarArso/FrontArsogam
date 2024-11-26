bd3f1f4b8782d05c5e8ca7ed1a8840ba449d06aa
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-11-26 00:11:02 -0500

    FrontCors

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,9 @@ import axios from "axios";
 const apiClient = axios.create({
     baseURL : 'https://backarsogam-production.up.railway.app',
     withCredentials : true,    
+    headers : {
+        'Content-Type': ['multipart/form-data',"application/json"],            
+    }
 })
 
 export default apiClient
