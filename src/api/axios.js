4de42ed13af526d90c2b6db05eb0d325fe111632
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-11-25 23:49:14 -0500

    FrontCnnfig

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,11 +3,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     baseURL : 'https://backarsogam-production.up.railway.app',
-    withCredentials : true,
-    headers : {
-        'Access-Control-Allow-Origin': 'https://arsogam.netlify.app',
-        'Content-Type': 'application/json',
-    }
+    withCredentials : true,    
 })
 
 export default apiClient
